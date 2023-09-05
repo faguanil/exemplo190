e183fafdaf68681cb92a5089a837df42ac060e77
faguanil <faguanil@gmail.com>
2023-09-05 16:24:22 -0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        //useMaterial3: true,
-      ),
-      home: const Center(
-        child: Text(
-          'Olá Mundos',
-          style: TextStyle(
-              fontSize: 80,
-              fontWeight: FontWeight.bold,
-              color: Colors.green,
-              letterSpacing: 8,
-              wordSpacing: 20),
+        // Define the primary color for the app
+        primaryColor: Colors.blue,
+
+        // Define the text theme with custom font and styles
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+              fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.green),
+          headline5: TextStyle(
+              fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.green),
         ),
+
+        // Define other properties like button styles, etc.
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
